ffc5166ccdc546226333d6730f2cbe0ed1391d9a
John Holdsworth <github@johnholdsworth.com>
2021-04-14 21:48:08 +0200

    gethostname?

--- Package.swift
+++ Package.swift
@@ -8,32 +8,32 @@ import Foundation
 
 //var addr = "localhost"
 
-@_silgen_name("htons")
-func htons(_ in: UInt16) -> UInt16
-@_silgen_name("htonl")
-func htonl(_ in: UInt32) -> UInt32
-public func rebind<IN,OUT>(_ x: UnsafeMutablePointer<IN>)
-    -> UnsafeMutablePointer<OUT> {
-    return x.withMemoryRebound(to: OUT.self, capacity: 1) { $0 }
-}
-
-let sock = socket(AF_INET, SOCK_STREAM, 0)
-if sock < 0 {
-    print("no sock")
-}
-var addr = sockaddr_in()
-addr.sin_len = UInt8(MemoryLayout.size(ofValue: addr))
-addr.sin_port = 0
-addr.sin_addr.s_addr = htonl(INADDR_ANY)
-if bind(sock, rebind(&addr), socklen_t(addr.sin_len)) < 0 {
-    print("no bind \(String(cString: strerror(errno)))")
-}
-if getsockname(sock, rebind(&addr), rebind(&addr.sin_len)) < 0 {
-    print("no name")
-}
-
-let ip = String(cString: inet_ntoa(addr.sin_addr))
-print(ip)
+//@_silgen_name("htons")
+//func htons(_ in: UInt16) -> UInt16
+//@_silgen_name("htonl")
+//func htonl(_ in: UInt32) -> UInt32
+//public func rebind<IN,OUT>(_ x: UnsafeMutablePointer<IN>)
+//    -> UnsafeMutablePointer<OUT> {
+//    return x.withMemoryRebound(to: OUT.self, capacity: 1) { $0 }
+//}
+//
+//let sock = socket(AF_INET, SOCK_STREAM, 0)
+//if sock < 0 {
+//    print("no sock")
+//}
+//var addr = sockaddr_in()
+//addr.sin_len = UInt8(MemoryLayout.size(ofValue: addr))
+//addr.sin_port = 0
+//addr.sin_addr.s_addr = htonl(INADDR_ANY)
+//if bind(sock, rebind(&addr), socklen_t(addr.sin_len)) < 0 {
+//    print("no bind \(String(cString: strerror(errno)))")
+//}
+//if getsockname(sock, rebind(&addr), rebind(&addr.sin_len)) < 0 {
+//    print("no name")
+//}
+//
+//let ip = String(cString: inet_ntoa(addr.sin_addr))
+//print(ip)
 
 //let pipe = Pipe()
 //let proc = Process()
@@ -51,7 +51,10 @@ print(ip)
 //    "^[^^]*inet\\s(\\S+)\\s[^^]*$",
 //     with: "$1", options: .regularExpression)
 //}
-print(addr)
+
+var hostname = [Int8](repeating: 0, count: 200)
+gethostname(&hostname, hostname.count)
+print(hostname)
 
 let package = Package(
     name: "RemotePlugin",
