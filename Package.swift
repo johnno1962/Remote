2216602ce09e791f35f94d1411301d7a723fcb1c
John Holdsworth <github@johnholdsworth.com>
2021-04-29 10:52:34 +0200

    Use Host.current().name (#14)

--- Package.swift
+++ Package.swift
@@ -1,14 +1,12 @@
 // swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-//  $Id: //depot/Remote/Package.swift#8 $
+//  $Id: //depot/Remote/Package.swift#10 $
 //
 
 import PackageDescription
 import Foundation
 
-var buffer = [Int8](repeating: 0, count: 200)
-gethostname(&buffer, buffer.count)
-let hostname = String(cString: buffer)
+let hostname = Host.current().name ?? "localhost"
 
 let package = Package(
     name: "RemotePlugin",
